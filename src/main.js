928ada9b0ca375644c6a68224e70294607e7dbbd
Guest User <Guest@epicodus-79.local>
2019-06-17 10:33:39 -0700

    revise spec code

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
-// import { myCode } from './myCode';
 
 $().ready(function(){
-  
+
 });
